b0486f70505fc4f4a512da9704e859ddb0ceee1b
Macros Meng <Meng-yc@outlook.com>
2025-08-26 20:19:14 +0800

    restructured code

---
+++ pylamina.py
@@ -0,0 +1,14 @@
+import sys
+import runtime
+
+
+cmd_args = sys.argv
+if len(cmd_args) == 1:
+    runtime.run_file(cmd_args[1])
+elif len(cmd_args) == 0:
+    runtime.repl()
+else:
+    print("Usage: pylamina.py [file_path]\n"
+          "    No arguments: Enter REPL mode\n"
+          "    With arguments: Run from the Lamina source code")
+    sys.exit(1)
---
+++ runtime.py
@@ -0,0 +1,6 @@
+def run_file(fp: str) -> None:
+    raise NotImplementedError
+
+
+def repl() -> None:
+    raise NotImplementedError
